20b98a56793506d8652caa766170e29c827d9f85
Mtamayo2104 <miguel.tamayo2104@gmail.com>
2025-01-15 18:45:52 +0100

    arith op

--- Arithmetic_operators.py
+++ Arithmetic_operators.py
@@ -1,4 +1,9 @@
 print(2+6)
 print(9-99)
 print(3*8)
-print(20/5)
+print(20/5) # always produces float result
+print(22/5)
+print(20//5) # this is the integer division
+print(22%5) # remainder of the division
+print(10%6)
+print(2**3) # power function
